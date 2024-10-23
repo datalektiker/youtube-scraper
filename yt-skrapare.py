6c6989b7064cc9bc2e431d746d00c1fcf8a23537
David Jönsson <datalektiker@gmail.com>
2024-10-23 21:14:43 +0200

    winfix

--- yt-skrapare.py
+++ yt-skrapare.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QTextEdit, QPushButton, QVBoxLayout, QWidget, QProgressBar,
-                             QHBoxLayout, QLabel, QLineEdit, QFormLayout, QMessageBox)
-from PyQt5.QtCore import QThread, pyqtSignal, QObject, Qt
+                             QHBoxLayout, QLineEdit, QFormLayout, QMessageBox)
+from PyQt5.QtCore import QThread, pyqtSignal, QObject
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
